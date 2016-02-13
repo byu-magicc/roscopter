1e4ddac2cc16b96f80a6326fc67254088d821b79
James Jackson <james.s.jackson@byu.edu>
2016-02-12 18:50:44 -0700

    moved matlab tuning out of the repository for now

--- matlab_tuning/attitude_test.m
+++
@@ -1,9 +0,0 @@
-data = processAllTopics('/home/jarvis/attitude_test.bag');
-
-%% accelerometers
-figure(4); clf; hold on;
-[b,a] = butter(6, 0.1);
-for i = 1:3
-     plot(data.imu.data.time, filtfilt(b,a,data.imu.data.euler(:,i)));
-end
-hold off
