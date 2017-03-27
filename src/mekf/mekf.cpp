d08b1aafc0e82895c6fc10763f3ff7fcfe826a7f
Jerel Nielsen <jerel.nielsen@gmail.com>
2017-03-27 14:19:28 -0600

    prediction is working correctly

--- src/mekf/mekf.cpp
+++ src/mekf/mekf.cpp
@@ -27,7 +27,6 @@ kalmanFilter::kalmanFilter() :
   is_flying_pub_ = nh_.advertise<std_msgs::Bool>("is_flying", 1);
 
   // initialize variables
-  x_hat_.setZero();
   flying_ = false;
 }
 
@@ -36,8 +35,10 @@ kalmanFilter::kalmanFilter() :
 void kalmanFilter::imuCallback(const sensor_msgs::Imu msg)
 {
   // wait for sufficient acceleration to start the filter
-  if(!flying_){
-    if(fabs(msg.linear_acceleration.z) > 10.0){
+  if(!flying_)
+  {
+    if(fabs(msg.linear_acceleration.z) > 10.0)
+    {
       // filter initialization stuff
       ROS_WARN("Now flying!");
       flying_ = true;
@@ -73,6 +74,13 @@ void kalmanFilter::predictStep()
     x_hat_ += (dt/N_)*f(x_hat_); // eq. 70
     P_ += (dt/N_)*(F*P_ + P_*F.transpose() + Gu*Qu_*Gu.transpose() + Qx_); // eq. 79
   }
+
+  // normalize quaternion portion of state
+  double x_hat_q_norm = sqrt(x_hat_(QX)*x_hat_(QX) + x_hat_(QY)*x_hat_(QY) + x_hat_(QZ)*x_hat_(QZ) + x_hat_(QW)*x_hat_(QW));
+  x_hat_(QX) /= x_hat_q_norm;
+  x_hat_(QY) /= x_hat_q_norm;
+  x_hat_(QZ) /= x_hat_q_norm;
+  x_hat_(QW) /= x_hat_q_norm;
   
   // store time for time step computation on the next iteration
   previous_time_ = current_time_;
@@ -176,54 +184,54 @@ Eigen::Matrix<double, NUM_ERROR_STATES, 6> kalmanFilter::dfdu(const Eigen::Matri
 // update state using accelerometer x and y measurements, Section 4.3.1
 void kalmanFilter::updateIMU(sensor_msgs::Imu msg)
 {
-  // collect measurements
+  // collect IMU measurements
   ygx_ = msg.angular_velocity.x;
   ygy_ = msg.angular_velocity.y;
   ygz_ = msg.angular_velocity.z;
   yax_ = msg.linear_acceleration.x;
   yay_ = msg.linear_acceleration.y;
   yaz_ = msg.linear_acceleration.z;
+  
+  // // build measurement vector (only use x and y)
+  // Eigen::Matrix<double, 2, 1> y;
+  // y << yax_, yay_;
 
-  // build measurement vector (only use x and y)
-  Eigen::Matrix<double, 2, 1> y;
-  y << yax_, yay_;
+  // // unpack the input
+  // double u(x_hat_(U)), v(x_hat_(V)), w(x_hat_(W));
+  // double p_hat(ygx_-x_hat_(GX)), q_hat(ygy_-x_hat_(GY)), r_hat(ygz_-x_hat_(GZ));
+  // double bax(x_hat_(AX)), bay(x_hat_(AY));
 
-  // unpack the input
-  double u(x_hat_(U)), v(x_hat_(V)), w(x_hat_(W));
-  double p_hat(ygx_-x_hat_(GX)), q_hat(ygy_-x_hat_(GY)), r_hat(ygz_-x_hat_(GZ));
-  double bax(x_hat_(AX)), bay(x_hat_(AY));
+  // // measurement Jacobian, eq. 115
+  // Eigen::Matrix<double, 2, NUM_ERROR_STATES> H;
+  // H << 0, 0, 0, 0, 0, 0,      0,  r_hat, -q_hat,  0,  w, -v, 1, 0, 0,
+  //      0, 0, 0, 0, 0, 0, -r_hat,      0,  p_hat, -w,  0,  u, 0, 1, 0;
 
-  // measurement Jacobian, eq. 115
-  Eigen::Matrix<double, 2, NUM_ERROR_STATES> H;
-  H << 0, 0, 0, 0, 0, 0,      0,  r_hat, -q_hat,  0,  w, -v, 1, 0, 0,
-       0, 0, 0, 0, 0, 0, -r_hat,      0,  p_hat, -w,  0,  u, 0, 1, 0;
+  // // input noise selector, eq. 118
+  // Eigen::Matrix<double, 2, 6> J;
+  // J <<  0,  w, -v, 1, 0, 0,
+  //      -w,  0,  u, 0, 1, 0;
 
-  // input noise selector, eq. 118
-  Eigen::Matrix<double, 2, 6> J;
-  J <<  0,  w, -v, 1, 0, 0,
-       -w,  0,  u, 0, 1, 0;
+  // // accelerometer noise matrix for x and y
+  // // input noise selector, eq. 118
+  // Eigen::Matrix<double, 2, 2> R;
+  // R << Qu_(3,3), 0, 0, Qu_(4,4);
 
-  // accelerometer noise matrix for x and y
-  // input noise selector, eq. 118
-  Eigen::Matrix<double, 2, 2> R;
-  R << Qu_(3,3), 0, 0, Qu_(4,4);
+  // // compute Kalman gain, eq. 117
+  // Eigen::Matrix<double, NUM_ERROR_STATES, 2> K;
+  // K = P_*H.transpose()*(H*P_*H.transpose() + J*Qu_*J.transpose() + R).inverse();
 
-  // compute Kalman gain, eq. 117
-  Eigen::Matrix<double, NUM_ERROR_STATES, 2> K;
-  K = P_*H.transpose()*(H*P_*H.transpose() + J*Qu_*J.transpose() + R).inverse();
+  // // compute measurement error below, eq. 114
+  // Eigen::Matrix<double, 2, 1> r;
+  // r << yax_ - (bax - q_hat*w + r_hat*v),
+  //      yay_ - (bay + p_hat*w - r_hat*u);
 
-  // compute measurement error below, eq. 114
-  Eigen::Matrix<double, 2, 1> r;
-  r << yax_ - (bax - q_hat*w + r_hat*v),
-       yay_ - (bay + p_hat*w - r_hat*u);
+  // // compute delta_x, eq. 88
+  // Eigen::Matrix<double, NUM_ERROR_STATES, 1> delta_x;
+  // delta_x = K*r;
 
-  // compute delta_x, eq. 88
-  Eigen::Matrix<double, NUM_ERROR_STATES, 1> delta_x;
-  delta_x = K*r;
-
-  // update state and covariance
-  stateUpdate(delta_x);
-  P_ = (Eigen::MatrixXd::Identity(NUM_ERROR_STATES,NUM_ERROR_STATES) - K*H)*P_;
+  // // update state and covariance
+  // stateUpdate(delta_x);
+  // P_ = (Eigen::MatrixXd::Identity(NUM_ERROR_STATES,NUM_ERROR_STATES) - K*H)*P_;
 }
 
 
