bf7417cffbf9f3a25e8ef66d04f3e0610a6ef16b
matt <matthewk.rydalch@gmail.com>
2020-07-06 15:39:27 -0600

    Added a better model of gps noise

--- roscopter/scripts/mocap2ublox.py
+++ roscopter/scripts/mocap2ublox.py
@@ -26,6 +26,9 @@ class Mocap2Ublox():
         self.lpf_on = lo
         self.A = A
         self.B = B
+        self.Kgps = 1100
+        self.eta_h = 0.03
+        self.eta_a = 0.06
 
         #calc ref_ecef from ref_lla
         self.F = (self.A - self.B)/self.A     # Ellipsoid Flatness
@@ -36,6 +39,7 @@ class Mocap2Ublox():
         self.ned_std_dev_3d = [self.global_horizontal_accuracy, self.global_horizontal_accuracy, self.global_vertical_accuracy]
         self.vel_std_dev_3d = [self.global_speed_accuracy, self.global_speed_accuracy, self.global_speed_accuracy]
         self.relpos_std_dev_3d = [self.relative_horizontal_accuracy, self.relative_horizontal_accuracy, self.relative_vertical_accuracy]
+        self.white_noise_3d = [self.eta_h, self.eta_h, self.eta_a]
 
         #other needed variables and arrays
         self.rover_ned = np.zeros(3)
@@ -49,6 +53,12 @@ class Mocap2Ublox():
         self.base_ned_lpf = np.zeros(3)
         self.base_vel_lpf = np.zeros(3)
         self.base_prev_time = 0.0
+        self.rover_ned_noise = np.zeros(3)
+        self.rover_vel_noise = np.zeros(3)
+        self.rover_vel_noise_prev = np.zeros(3)
+        self.base_ned_noise = np.zeros(3)
+        self.base_vel_noise = np.zeros(3)
+        self.base_vel_noise_prev = np.zeros(3)
 
         #Outputs
         self.rover_virtual_pos_ecef = np.zeros(3)
@@ -61,9 +71,9 @@ class Mocap2Ublox():
     def update_rover_virtual_PosVelEcef(self, dt):
 
         #calculate virtual position in ecef frame with noise
-        rover_ned_noise = self.add_noise_3d(self.rover_ned, self.ned_std_dev_3d)
-        self.rover_ned_lpf = self.lpf(rover_ned_noise, self.rover_ned_lpf, self.Ts, self.sigma_rover_pos)
-        virtual_delta_ecef = self.ned2ecef(self.rover_ned_lpf, self.ref_lla)
+        self.rover_ned_noise = self.add_gps_noise(self.white_noise_3d, self.rover_ned_noise, self.sigma_rover_pos)
+        rover_ned_w_noise = self.rover_ned + self.rover_ned_noise 
+        virtual_delta_ecef = self.ned2ecef(rover_ned_w_noise, self.ref_lla)
         self.rover_virtual_pos_ecef = self.ref_ecef + virtual_delta_ecef
 
         #calculate virtual velocity in ecef frame with noise
@@ -72,13 +82,16 @@ class Mocap2Ublox():
             rover_vel = (self.rover_ned - self.rover_ned_prev)/dt
         else:
             rover_vel = np.zeros(3)
-        rover_vel_noise = self.add_noise_3d(rover_vel, self.vel_std_dev_3d)
-        self.rover_vel_lpf = self.lpf(rover_vel_noise, self.rover_vel_lpf, self.Ts, self.sigma_rover_vel)
-        self.rover_virtual_vel_ecef = self.ned2ecef(rover_vel_noise, self.ref_lla)
+        self.rover_vel_noise = self.add_noise_3d(np.zeros(3), self.white_noise_3d)
+        self.rover_vel_lpf = self.lpf(self.rover_vel_noise, self.rover_vel_noise_prev, self.Ts, self.sigma_rover_vel)
+        rover_vel_w_noise = rover_vel + self.rover_vel_lpf
+        self.rover_virtual_vel_ecef = self.ned2ecef(rover_vel_w_noise, self.ref_lla)
 
         #update histories
         self.rover_ned_prev = self.rover_ned
         self.rover_vel_prev = rover_vel
+        self.rover_vel_noise_prev = self.rover_vel_noise
+
 
 
     def update_rover_virtual_relPos(self):
@@ -94,9 +107,9 @@ class Mocap2Ublox():
     def update_base_virtual_PosVelEcef(self, dt):
 
         #calculate virtual position in ecef frame with noise
-        base_ned_noise = self.add_noise_3d(self.base_ned, self.ned_std_dev_3d)
-        self.base_ned_lpf = self.lpf(base_ned_noise, self.base_ned_lpf, self.Ts, self.sigma_base_pos)
-        virtual_delta_ecef = self.ned2ecef(self.base_ned_lpf, self.ref_lla)
+        self.base_ned_noise = self.add_gps_noise(self.white_noise_3d, self.base_ned_noise, self.sigma_base_pos)
+        base_ned_w_noise = self.base_ned + self.base_ned_noise
+        virtual_delta_ecef = self.ned2ecef(base_ned_w_noise, self.ref_lla)
         self.base_virtual_pos_ecef = self.ref_ecef + virtual_delta_ecef
 
         #calculate virtual velocity in ecef frame with noise
@@ -105,13 +118,15 @@ class Mocap2Ublox():
             base_vel = (self.base_ned - self.base_ned_prev)/dt
         else:
             base_vel = np.zeros(3)
-        base_vel_noise = self.add_noise_3d(base_vel, self.vel_std_dev_3d)
-        self.base_vel_lpf = self.lpf(base_vel_noise, self.base_vel_lpf, self.Ts, self.sigma_base_vel)
-        self.base_virtual_vel_ecef = self.ned2ecef(base_vel_noise, self.ref_lla)
+        self.base_vel_noise = self.add_noise_3d(np.zeros(3), self.white_noise_3d)
+        self.base_vel_lpf = self.lpf(self.base_vel_noise, self.base_vel_noise_prev, self.Ts, self.sigma_base_vel)
+        base_vel_w_noise = base_vel + self.base_vel_lpf
+        self.base_virtual_vel_ecef = self.ned2ecef(base_vel_w_noise, self.ref_lla)
 
         #update histories
         self.base_ned_prev = self.base_ned
         self.base_vel_prev = base_vel
+        self.base_vel_noise_prev = self.base_vel_noise
 
 
     def add_noise_3d(self, value, std_dev):
@@ -126,6 +141,16 @@ class Mocap2Ublox():
         else:
             return value
 
+
+    def add_gps_noise(self, white_noise_3d, noise_prev, sigma):
+
+        #Eq 7.17 Small Unmanned Aircraft (Beard, McLain) pg 139
+        white_noise = self.add_noise_3d(np.zeros(3), self.white_noise_3d)
+        color_noise = np.exp(-self.Kgps*self.Ts)*noise_prev
+        total_noise = white_noise + color_noise
+        noise = self.lpf(total_noise, noise_prev, self.Ts, sigma)
+
+        return noise
 
     def lpf(self, xt, x_prev, dt, sigma):
         
