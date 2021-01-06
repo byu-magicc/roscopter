d1ec474f62017d52ddd762331df6d82d2838ebc3
David <davidc@linuxpopper.org>
2021-01-06 14:13:23 -0700

    created trajectory_tracker.h file

---
+++ roscopter/include/trajectory_tracker/trajectory_tracker.h
@@ -0,0 +1,14 @@
+#ifndef TRAJECTORY_TRACKER_H
+#define TRAJECTORY_TRACKER_H
+
+// #include <ros/ros.h>
+// #include <roscopter_msgs/Trajectory_Command.h>
+// #include <rosflight_msgs/Command.h>
+// #include <rosflight_msgs/Status.h>
+// #include <nav_msgs/Odometry.h>
+
+namespace trajectory_tracker
+{
+    
+}
+
